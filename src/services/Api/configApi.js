19d347cdf327cefdd03350f66444ec02c3239726
dvcuong97 <cuonggdv08@gmail.com>
2023-11-13 18:41:32 +0700

    fix

--- src/services/Api/configApi.js
+++ src/services/Api/configApi.js
@@ -10,7 +10,7 @@ export const https = axios.create({
   baseURL: BASE_URL,
   headers: {
     TokenCybersoft: TOKEN_CYBERSOFT,
-    Authorization: "Bearer " + userLocalStorage.get().accessToken,
+    Authorization: "Bearer " + userLocalStorage.get()?.accessToken,
   },
 });
 
